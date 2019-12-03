ebeac541afb240f2ccb4e0b40bc84441c565894c
Jayden Chan <jaydencn7@gmail.com>
2019-12-03 13:43:29 -0800

    JC: Update JSON command

--- src/cmd/json.ts
+++ src/cmd/json.ts
@@ -1,23 +1,34 @@
 import { generate as generateJson } from "../util/json";
 import { isDir, gherkins } from "../util/fio";
 import { Arguments } from "yargs";
+import { writeFileSync } from "fs";
 
-export const command = "json";
+export const command = "json <input> [out]";
 export const desc = "Parse the provided file or directory into JSON";
 
+export const builder = (yargs: any) => {
+  return yargs
+    .positional("input", {
+      describe: "Input feature file or directory",
+      type: "file"
+    })
+    .positional("out", {
+      describe:
+        "Output file or directory. Will print to STDOUT if not specified",
+      type: "file"
+    });
+};
+
 export const handler = async (argv: Arguments) => {
-  argv._.shift();
-
-  const args = argv._;
-
-  if (args.length !== 1) {
-    console.log("Wrong number of arguments.");
-    console.log("Pass a .feature file or directory.");
-    return;
-  }
-
-  const files = isDir(args[0]) ? gherkins(args[0]) : [args[0]];
+  const inFile = argv.input as string;
+  const outFile = argv.out as string;
+  const files = isDir(inFile) ? gherkins(inFile) : [inFile];
 
   const json = await generateJson(files);
-  console.log(JSON.stringify(json, undefined, 2));
+
+  if (outFile !== undefined) {
+    writeFileSync(outFile, JSON.stringify(json, undefined, 2));
+  } else {
+    console.log(JSON.stringify(json, undefined, 2));
+  }
 };
